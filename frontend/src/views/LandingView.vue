1ac784598131259bbcb57a1f206e91fa5185f0c0
Ritik Soni <23f3003280@ds.study.iitm.ac.in>
2026-06-25 16:51:04 +0000

    working on frontend

---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,31 @@
+<template>
+<h2> welcome </h2>
+<div>
+
+
+    <div>
+        <div @click="$router.push('/admin')">
+            Admin View
+        </div>
+    </div>
+
+
+    <div>
+        <div @click="$router.push('/user')">
+            User View
+        </div>
+    </div>
+
+</div>
+
+</template>
+
+
+
+<script>
+export default {
+    name: 'LandingView'
+}
+</script>
+
+
